68256b2844fea7a8d608f4bc7be64d1ca7ac11ef
Kaue_ocalixto <kaue.santos.calixto@gmail.com>
2025-05-19 21:15:33 -0300

    Tests jest

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,20 @@
 module.exports = {
-<<<<<<< HEAD
   database: {
     development: {
-      url: 'postgres://usuario:senha@localhost:5432/api_comunidade', 
-      dialect: 'postgres',
+      url: process.env.DB_URL || 'postgres://usuario:senha@localhost:5432/api_comunidade',
+      dialect: process.env.DB_DIALECT || 'postgres',
       logging: true,
       dialectOptions: {
         ssl: false,
       },
     },
+    test: {
+      url: 'sqlite::memory:',
+      dialect: 'sqlite',
+      logging: false,
+    },
     production: {
-      url: process.env.DATABASE_URL, 
+      url: process.env.DATABASE_URL,
       dialect: 'postgres',
       logging: false,
       dialectOptions: {
@@ -22,29 +26,3 @@ module.exports = {
     },
   },
 };
-=======
-    database: {
-      development: {
-        url: process.env.DB_URL || 'sqlite::memory:', 
-        dialect: process.env.DB_DIALECT || 'sqlite',
-        logging: true
-      },
-      test: {
-        url: 'sqlite::memory:',
-        dialect: 'sqlite',
-        logging: false
-      },
-      production: {
-        url: process.env.DATABASE_URL, 
-        dialect: 'postgres',
-        logging: false,
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        },
-      },
-    },
-  };
->>>>>>> origin/middlewares
